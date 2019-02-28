b3cd8cd6badc0b470c51b8bfeb8126b26136e4a0
唐成勇 <1946644259@qq.com>
2019-02-28 16:43:06 +0800

    Update Gitlab.sh

--- Gitlab.sh
+++ Gitlab.sh
@@ -1,7 +1,7 @@
 
-docker run --name='gitlab-test' -d \
+docker run --name='gitlab' -d \
        --net=gitlab_net \
-       --publish 8093:80 \
+       --publish 8090:80 \
        --restart always \
        --volume /root/docker/gitlab/config:/etc/gitlab \
        --volume /root/docker/gitlab/logs:/var/log/gitlab \
@@ -10,3 +10,14 @@ docker run --name='gitlab-test' -d \
        -e 'GITLAB_SSH_PORT=8093'\
        -e 'GITLAB_HOST=http://39.104.67.146'\
        gitlab/gitlab-ce:latest
+       
+       
+       
+ docker run --name='gitlab' -d \
+       --net=gitlab_net \
+       --publish 8090:80 \
+       --restart always \
+       --volume /root/docker/gitlab/config:/etc/gitlab \
+       --volume /root/docker/gitlab/logs:/var/log/gitlab \
+       --volume /root/docker/gitlab/data:/var/opt/gitlab 
+       gitlab/gitlab-ce:9.5.4-ce.0
