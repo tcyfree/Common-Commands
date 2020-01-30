888bd7905a1c680b1f68448e3cb937fa02076ba7
唐成勇 <1946644259@qq.com>
2020-01-30 20:31:02 +0800

    Update Gitlab&Gitlab-Runner.sh
    Steps of setting swap partition for ECS

--- Gitlab&Gitlab-Runner.sh
+++ Gitlab&Gitlab-Runner.sh
@@ -7,6 +7,53 @@
     --volume /root/docker/gitlab/logs:/var/log/gitlab \
     --volume /root/docker/gitlab/data:/var/opt/gitlab \
     gitlab/gitlab-ce:9.5.4-ce.0
+    
+Tips: 由于gitlab特别耗费内存，官方推荐2核4G。而阿里云ECS默认是没有swap交换分区，故需要手动创建：https://www.imydl.tech/lnmp/240.html
+这个 bs 的单位可以用 man dd 进行查看，有详细的说明，我这里创建 4G
+dd if=/dev/zero of=/mnt/swapfile bs=1MB count=4096
+
+这里把刚才创建的文件做成swap文件
+    mkswap /mnt/swapfile
+
+开启swap
+    swapon /mnt/swapfile
+
+查看状态
+    swapon -s
+
+查看内存状态
+    free -h
+
+=========以下操作能确保 swap 更安全，且重启后有效===========
+
+查看 vim /etc/rc.local 如果有 swapoff -a 修改为 swapon -a
+
+    vim /etc/rc.local
+
+设置自动挂载
+    vim /etc/fstab
+
+    /mnt/swapfile swap swap defaults 0 0
+
+权限设置
+
+    chown root:root /mnt/swapfile
+
+    chmod 0600 /mnt/swapfile
+
+查看 swappiness 值。
+    cat /proc/sys/vm/swappiness
+
+    如果为 0 ，则将其改为 10
+
+    sysctl vm.swappiness=10
+
+永久设置 swappiness，如果该文件里没有，则追加 vm.swappiness = 10
+    vim /etc/sysctl.conf
+
+重启，那些永久设置项就生效了。
+    PS：swappiness=0 的时候表示最大限度使用物理内存，然后才是 swap 空间，swappiness＝100的时候表示积极的使用swap分区，并且把内存上的数据及时的搬运到swap空间里面。上述都是亲测有效的。
+
 #二、安装gitlab-runner
 
     sudo docker pull gitlab/gitlab-runner:latest
